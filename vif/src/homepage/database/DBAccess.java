76c453ae961a9d819558e4d3b49ac57967391074
bakhoj <anders2108@gmail.com>
2014-11-03 13:47:53 +0100

    Retter i DBAccess

--- vif/src/homepage/database/DBAccess.java
+++ vif/src/homepage/database/DBAccess.java
@@ -1,7 +1,20 @@
 package homepage.database;
 
+import java.sql.*;
+
 public class DBAccess {
+	private Connection connect					= null;
+	private Statement statement					= null;
+	private ResultSet resultSet					= null;
+	
+	private String DBHost;
+	private int DBPort;
+	private String DBDatabase;
+	private String DBUserName;
+	private String DBPassword;
+	
 	public DBAccess() {
+		
 		System.out.println("init");
 	}
 }
